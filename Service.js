ed28abe046015a1e0cfbf4ad46663603a36ae7d9
smallclown <806343718@qq.com>
2017-01-15 17:11:55 +0800

    Add files via upload
    瞅瞅吧 以前写的 今天疯狂上传了感觉

---
+++ Service.js
@@ -0,0 +1,8 @@
+$(function () {
+	//流程移入背景颜色改变
+	$(".js_liucheng_item").mouseover(function (e) {
+		$(this).find(".js_bg_l1").fadeIn(200).siblings(".js_bg_l2").fadeOut(200);
+	}).mouseout(function() {
+		$(this).find(".js_bg_l1").fadeOut(200).siblings(".js_bg_l2").stop().fadeIn(200);
+	});
+});
