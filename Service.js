844db31b09fdf06fbee3c15b3848ff447107ad5e
smallclown <smallclown-vip@qq.com>
2017-03-10 18:57:07 +0800

    Update Service.js

--- Service.js
+++ Service.js
@@ -1,6 +1,6 @@
 $(function () {
-	//流程移入背景颜色改变
-	$(".js_liucheng_item").mouseover(function (e) {
+	//流程移入背景颜色改变	
+$(".js_liucheng_item").mouseover(function (e) {
 		$(this).find(".js_bg_l1").fadeIn(200).siblings(".js_bg_l2").fadeOut(200);
 	}).mouseout(function() {
 		$(this).find(".js_bg_l1").fadeOut(200).siblings(".js_bg_l2").stop().fadeIn(200);
